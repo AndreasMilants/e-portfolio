f3dee077d185ab3f7fe60d24c6c94046de84333d
r0668522 <andreas.milants@student.ucll.be>
2020-03-18 11:24:30 +0100

    Updates

---
+++ precache-manifest.80874944881c1c08f4937fdeb215673c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e52ecbbe2d69f12f9d6c961185f63d",
+    "url": "/e-portfolio/index.html"
+  },
+  {
+    "revision": "0fd012907cd4c22cb135",
+    "url": "/e-portfolio/static/css/main.3d2dfee6.chunk.css"
+  },
+  {
+    "revision": "6dcdaff49f937cbb5f5f",
+    "url": "/e-portfolio/static/js/2.ea60418b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/e-portfolio/static/js/2.ea60418b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd012907cd4c22cb135",
+    "url": "/e-portfolio/static/js/main.dc7ef6ac.chunk.js"
+  },
+  {
+    "revision": "78bd6997614f233a03ec",
+    "url": "/e-portfolio/static/js/runtime-main.bdd69df9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d294a9799c7ba1dadaa27589339fe3b.js"
+  "/e-portfolio/precache-manifest.80874944881c1c08f4937fdeb215673c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
